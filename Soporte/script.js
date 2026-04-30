270ea71c68b4005bf06904aa3b3ca4fab9eeee00
AlanGamerTorre <alangamertorre13@gmail.com>
2026-04-30 16:17:42 +0200

    Add files via upload

--- Soporte/script.js
+++ Soporte/script.js
@@ -1,4 +1,7 @@
 const barra = document.querySelector(".barra");
+const caja_txt_barra = document.querySelector(".caja-txt");
+const menu_desplegable = document.getElementById("menu-desplegable-barra");
+const lado_izquierdo = document.querySelector(".lado-izquierdo");
 const txt_inicio = document.querySelector(".txt-con-pri");
 const img_inicio = document.querySelector(".img-soporte");
 const opciones = document.querySelectorAll(
@@ -220,3 +223,76 @@ async function enviarMensajeIA() {
 document.getElementById("ia-input").addEventListener("keydown", (e) => {
   if (e.key === "Enter") enviarMensajeIA();
 });
+
+/*
+ *  Menú desplegable (movil)
+ */
+function movilePhone() {
+  let movilOptn = window.innerWidth <= 745 ? true : false;
+
+  const btns_barra = caja_txt_barra.querySelectorAll("a");
+  if (movilOptn === true) {
+    menu_desplegable.style.display = "flex";
+    btns_barra.forEach((btn) => {
+      btn.style.display = "none";
+      btn.style.opacity = 0;
+    });
+
+    let esAbierto = false;
+
+    menu_desplegable.addEventListener("click", () => {
+      esAbierto = !esAbierto;
+
+      const TL = anime.timeline({
+        easing: "easeInOutSine",
+        duration: 1000,
+      });
+
+      TL.add({
+        targets: lado_izquierdo,
+        opacity: esAbierto ? [1, 0] : [0, 1],
+        translateX: esAbierto ? [0, -500] : [-500, 0],
+        easing: "cubicBezier(0.68, -0.55, 0.27, 1.55)",
+        begin: () => {
+          caja_txt_barra.style.width = esAbierto ? "auto" : "";
+          caja_txt_barra.style.gap = esAbierto ? "10px" : "25px";
+          caja_txt_barra.style.position = esAbierto ? "fixed" : "";
+          btns_barra.forEach((btn) => {
+            btn.style.fontSize = esAbierto ? "0.75rem" : "1rem";
+            btn.style.display = esAbierto ? "flex" : "none";
+          });
+        },
+      })
+        .add(
+          {
+            targets: barra,
+            padding: esAbierto ? "0 10px" : "0 40px",
+          },
+          "-=800",
+        )
+        .add(
+          {
+            targets: btns_barra,
+            opacity: esAbierto ? [0, 1] : [1, 0],
+          },
+          "-=600",
+        );
+    });
+  } else {
+    menu_desplegable.style.display = "none";
+    lado_izquierdo.style.transform = "translateX(0)";
+    lado_izquierdo.style.opacity = 1;
+    barra.style.padding = "0 40px";
+    caja_txt_barra.style.position = "";
+    caja_txt_barra.style.width = "";
+    caja_txt_barra.style.gap = "25px";
+    btns_barra.forEach((btn) => {
+      btn.style.display = "flex";
+      btn.style.opacity = 1;
+      btn.style.fontSize = "1rem";
+    });
+  }
+}
+movilePhone();
+
+window.addEventListener("resize", () => movilePhone()); //detecta canvios de tamaño en window
