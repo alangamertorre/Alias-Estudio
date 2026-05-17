dca1792c8b3209c8d4ab4eadb33c61b36baa661d
AlanGamerTorre <alangamertorre13@gmail.com>
2026-05-17 13:40:04 +0200

    Update SO.js

--- Productos/SO.js
+++ Productos/SO.js
@@ -6,20 +6,16 @@ function getFileForOS(operativeSystem) {
   let file = "";
 
   if (operativeSystem === "Windows") {
-    file = "ZIP/Alias-Studio-Code-Windows_x64.msi";
+    file = "ZIP/Alias-Studio-Code-Windows_x64.exe";
   } else if (operativeSystem === "Linux") {
-    file = "ZIP/Alias-Studio-Code-Linux_x64.deb";
+    file = "ZIP/Alias-Studio-Code-Linux_x64.zip";
   } else if (operativeSystem === "MacOS") {
-    file = "ZIP/Alias-Studio-Code-MacOS_x64.dmg";
-  } else if (operativeSystem === "Android") {
-    file = "ZIP/Alias-Studio-Code-Android_x64.apk";
-  } else if (operativeSystem === "iOS") {
-    file = "ZIP/Alias-Studio-Code-iOS_x64.ipa";
+    file = "ZIP/Alias-Studio-Code-MacOS_x64.zip";
   } else {
     alert(
-      "Sistema operativo no reconocido. Por favor, del .zip descargue el archivo correspondiente a su sistema operativo.",
+      "Sistema operativo no reconocido o no compatible.",
     );
-    file = "ZIP/Alias-Studio-Code-UnknownOS_x64.zip";
+
   }
 
   return file;
